2ddf689c5c3951663106d86fbdb950f44e055367
ayyoobraheemvk <119760917+ayyoobraheemvk@users.noreply.github.com>
2022-12-11 22:04:36 +0530

    html and css

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP FORM</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="mainbody">
+        <div class="formbody">
+            <p>LOG IN</p>
+            <form method="post">
+                <table>
+                    <tr>
+                        <td><label class="login-label">User Name</label></td>
+                    </tr>
+                    <tr>
+                        <td> <input type="text" class="input"></td>
+                    </tr>
+                    <tr>
+                        <td><label class="login-label">Email</label></td>
+                    </tr>
+                    <tr>
+                        <td> <input type="email" class="input"></td>
+                    </tr>
+                    <tr>
+                        <td><label  class="login-label">password</label></td>
+                    </tr>
+                    <tr>
+                        <td><input type="password" name="" class="input"></td> 
+                     </tr>
+                     <tr> <td><input type="submit" value="Log in" class="submit"></td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
